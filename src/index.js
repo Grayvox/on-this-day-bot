098679003176613e09bc4420a72e0e323e51f5fa
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-05-30 15:19:04 -0400

    Add tsx launch.json configuation to VS Code

--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var discord_js_1 = require("discord.js");
-var config_json_1 = require("../config.json");
-var client = new discord_js_1.Client({ intents: [discord_js_1.GatewayIntentBits.Guilds] });
-client.once(discord_js_1.Events.ClientReady, function (readyClient) {
-    console.log("I'm online! Logged in as ".concat(readyClient.user.tag));
-});
-client.login(config_json_1.token);
