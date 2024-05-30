f21a76cfb9d7bdf8da8049f770f443a4166ba5a0
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-05-30 14:55:42 -0400

    Create Discord bot and read JSON files
    * Creates a client instance of the Discord bot
    
    * Adds resolveJsonModule to tsconfig

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { token } from '../config.json';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`I'm online! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(token);
