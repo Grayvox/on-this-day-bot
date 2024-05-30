1b7fe714eec52b314c5b2abe65c1120d86415bf3
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-05-30 16:47:31 -0400

    Add basic help command for testing
    Adds a help command to test command interactions.
    Will be rewritten later

---
+++ src/commands/help.ts
@@ -0,0 +1,10 @@
+import { Interaction, SlashCommandBuilder } from "discord.js";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Provides help to the user'),
+    async execute(interaction: any) {
+        await interaction.reply('It works!')
+    }
+}
